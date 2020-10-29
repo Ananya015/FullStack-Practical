3f08756a8c2708adce4356db7ebb4856529909e0
Ananya015 <jainananya78@gmail.com>
2020-10-29 23:01:59 +0530

    applying js

---
+++ JavaScript/pract-15.js
@@ -0,0 +1,7 @@
+var decre = document.getElementById("left-btn");
+var incre = document.getElementById("right-btn");
+
+function incre() {
+decre.disabled = true;
+decre.style.backgroundColor = "rgb(94, 134, 209)";
+}
