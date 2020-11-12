4e8e051383957b13da7533ae494a13b3a4460f7f
Ananya015 <jainananya78@gmail.com>
2020-11-12 23:54:01 +0530

    adding currenttime function

--- JavaScript/pract-17.js
+++ JavaScript/pract-17.js
@@ -1,4 +1,23 @@
 
-const time= document.getElementById('time');
-const greeting =document.getElementById('greeting');
+const time = document.getElementById('time');
+const greeting = document.getElementById('greeting');
 const name = document.getElementById('name');
+
+
+function currenttime(){
+    let today = new Date();
+    let hour = today.getHours();
+    let min = today.getMinutes();
+    let sec = today.getSeconds();
+
+
+    const ampM = hour > 12 ? 'PM' : 'AM';
+
+     hour = hour%12 || 12;
+     
+     time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec} ${ampM}`;
+
+     setTimeout(currenttime,1000)
+
+}
+currenttime();
