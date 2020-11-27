22cb0ff5a64a6f50ebc545dc4593954134f5981a
Ananya015 <jainananya78@gmail.com>
2020-11-27 22:37:55 +0530

    adding selectors in js file

---
+++ JavaScript/pract-18.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const pass1 = document.getElementById('pass1');
+const pass2 = document.getElementById('pass2');
+const small = document.getElementById('small');
+
+
+form.addEventListener('submit', (e)=>{
+
+e.preventDefault();
+
+
+}
+);
+
+function checkInput(){
+    const usernameVal= username.Value();
+    const emailnameVal = email.Value();
+    const pass1Value = pass1.Value();
+    const pass2Vlaue = pass2 .Value();
+    
+
+}
