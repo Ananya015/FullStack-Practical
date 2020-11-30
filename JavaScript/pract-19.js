9fa133d04b3fa26fe17dc5f95e408f9e9d686734
Ananya015 <jainananya78@gmail.com>
2020-11-30 15:52:35 +0530

    making selectors

---
+++ JavaScript/pract-19.js
@@ -0,0 +1,8 @@
+ const form1=document.getElementById('form1');
+ const form2=document.getElementById('form2');
+ const form3=document.getElementById('form3');
+
+ const next1=document.getElementById('next1');
+ const next2=document.getElementById('next2');
+ const back1=document.getElementById('back1');
+ const back2=document.getElementById('back2');
